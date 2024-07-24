86f052253adce8320ef0188c92cc79b2b0ac546b
Derek Meduri <derekmeduri@gmail.com>
2024-07-24 14:57:04 -0400

    add

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,13 +6,13 @@ const secret = process.env.SECRET;
 const expiration = "2h";
 
 module.exports = {
-  AuthenicationError: new GraphQLError("Could not authenticate user.", {
+  AuthenticationError: new GraphQLError("Could not authenticate user.", {
     extensions: {
-      code: "UNATHENTICATED",
+      code: "UNAUTHENTICATED",
     },
   }),
   authMiddleware: async ({ req }) => {
-    // allows token to be sent via  req.body or req.query or headers
+    // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
@@ -20,24 +20,27 @@ module.exports = {
       token = token.split(" ").pop().trim();
     }
 
+    // if no token, return the request object as is
     if (!token) {
       return req;
     }
 
-    // verify token and get user data out of it
+    // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      console.log("Verify JWT Data: ", data);
       req.user = data;
     } catch (err) {
       console.error("Invalid token");
     }
-    // return request object
+
+    // return the request object so it can be passed to the resolver as `context`
     console.log("Returned context: ", req);
     return req;
   },
 
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
